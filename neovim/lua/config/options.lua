5e6cf6045a57d513ada919e634099af6e85b3256
Michael Kwan <michaelkwan1238@gmail.com>
2024-07-02 10:02:45 -0400

    Update shell config to use pwsh

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -6,3 +6,8 @@ opt.clipboard = "" -- Do NOT sync with system clipboard
 opt.relativenumber = false -- Disable relative line numbers
 opt.smartindent = false -- Enable smart indentation.
 opt.laststatus = 2 -- show statusline for everything, not just 1, lualine overrides this to 3
+
+-- https://github.com/neovim/neovim/issues/13893 and :h shell-powershell for shell related issues
+opt.shell = "pwsh"
+opt.shellcmdflag = "-NoLogo -noprofile -ExecutionPolicy RemoteSigned -Command"
+opt.shellxquote = ""
