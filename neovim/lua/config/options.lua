afa6854386193eec61a1210ec1fa4804031b715f
Michael Kwan <michaelkwan1238@gmail.com>
2024-07-02 15:06:51 -0400

    Disable format on save by default leader uf still toggles it

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -2,11 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
+
 opt.clipboard = "" -- Do NOT sync with system clipboard
 opt.relativenumber = false -- Disable relative line numbers
 opt.smartindent = false -- Enable smart indentation.
 opt.laststatus = 2 -- show statusline for everything, not just 1, lualine overrides this to 3
+g.autoformat = false -- Disable format on save
 
+-- Enable using pwsh instead of cmd for shell commands
 -- https://github.com/neovim/neovim/issues/13893 and :h shell-powershell for shell related issues
 opt.shell = "pwsh"
 opt.shellcmdflag = "-NoLogo -noprofile -ExecutionPolicy RemoteSigned -Command"
